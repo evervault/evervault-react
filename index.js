e9d9cb687aa8d88b3e436f95dc8beb4cbba2d611
Liam Farrelly <liam@evervault.com>
2020-02-26 11:55:00 +0000

    Remove useEvervault hook for now

--- index.js
+++ index.js
@@ -1,34 +1,19 @@
-import * as React from 'react';
+import React from 'react';
 import evervault from '@evervault/sdk'
 
 export const EvervaultContext = React.createContext(undefined);
 export const EvervaultProvider = EvervaultContext.Provider;
 export const EvervaultConsumer = EvervaultContext.Consumer;
 
-export function useEvervault(){
-  const evervault = React.useContext(EvervaultContext);
-  if(!evervault){
-    throw new Error('evervault context could not be found. Please ensure that you have declared an evervault provider');
-  }
-  return evervault;
-}
-
 export function withEvervault(WrappedComponent, APP_ID, AUTH_URL, API_URL, useEvervaultContext) {
   return class extends React.Component {
-    state = {
-      evervault: null
-    }
-    
     constructor(props) {
       super(props);
       evervault.init({auth: AUTH_URL, api: API_URL});
       evervault.checkAuth(APP_ID);
-    }
-
-    componentDidMount() {
-      this.setState({
-        evervault
-      })
+      this.state = { 
+        evervault 
+      };
     }
     
     render() {
