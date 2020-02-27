4eb385db39dc65a551a81b60a46ce10378ae8681
Patrick Monahan <patrickmonahan@Patricks-MacBook-Pro-2.local>
2020-02-27 12:42:16 +0000

    Update appId sdk input

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ export function withEvervault(WrappedComponent, params) {
   return class extends React.Component {
     constructor(props) {
       super(props);
-      evervault.init({auth: authUrl, api: apiUrl}, { appId: appId });
+      evervault.init({auth: authUrl, api: apiUrl}, { appId });
       evervault.checkAuth(appId);
       this.state = { 
         evervault 
