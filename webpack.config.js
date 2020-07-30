e6990655a7e5a1ee3ac94ee293c5ee767e8cb356
Eoin Boylan <jb.eoin@gmail.com>
2020-07-30 16:13:26 +0100

    Add bable regen polyfil

--- webpack.config.js
+++ webpack.config.js
@@ -18,8 +18,9 @@ module.exports = {
         options: {
           presets: ['@babel/preset-env', '@babel/react'],
           plugins: [
-            "@babel/plugin-proposal-class-properties",
-            '@babel/plugin-transform-react-jsx'
+            '@babel/plugin-proposal-class-properties',
+            '@babel/plugin-transform-react-jsx',
+            '@babel/plugin-transform-regenerator'
           ]
         }
       },
